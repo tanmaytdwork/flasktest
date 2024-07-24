99d7d7c6757033ca1a0f85876868cb5e048833e6
Tanmay Deshmukh <Tanmay.Deshmukh@DC-455.local>
2024-07-25 00:10:37 +0530

    update

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
+
+
+@app.get("/data")
+def get_data():
+    return{"data":"this is all the data"}
+
+@app.get("/")
+def get_data():
+    return{"data":"this is home page"}
